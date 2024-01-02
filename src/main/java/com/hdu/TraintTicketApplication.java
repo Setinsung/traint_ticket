f6555d46472cd0a8fecfad5e4d6d37b6a4e6f4d2
Setinsung <1968989251@qq.com>
2024-01-02 16:06:15 +0800

    数源配置

--- src/main/java/com/hdu/TraintTicketApplication.java
+++ src/main/java/com/hdu/TraintTicketApplication.java
@@ -3,8 +3,10 @@ package com.hdu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableTransactionManagement // 开启事务注解
 public class TraintTicketApplication {
 
     public static void main(String[] args) {
